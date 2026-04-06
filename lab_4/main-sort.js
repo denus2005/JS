e97d0ded404f389734c8818e05dff76578488fe7
denus2005 <150010370+denus2005@users.noreply.github.com>
2026-04-06 13:27:16 +0300

    Update main-sort.js

--- lab_4/main-sort.js
+++ lab_4/main-sort.js
@@ -1,1 +1,28 @@
+function generateArray(size) {
+    return Array.from({ length: size }, () =>
+        Math.floor(Math.random() * 1000)
+    );
+}
 
+// звичайний масив
+let normalArray = generateArray(100);
+
+// розріджений масив
+let sparseArray = generateArray(100);
+sparseArray[10] = undefined;
+sparseArray[40] = undefined;
+sparseArray[90] = undefined;
+
+console.log("=== NORMAL ARRAY ===");
+SortLib.bubbleSort(normalArray);
+SortLib.selectionSort(normalArray);
+SortLib.insertionSort(normalArray);
+SortLib.shellSort(normalArray);
+SortLib.quickSort(normalArray);
+
+console.log("=== SPARSE ARRAY ===");
+SortLib.bubbleSort(sparseArray);
+SortLib.selectionSort(sparseArray);
+SortLib.insertionSort(sparseArray);
+SortLib.shellSort(sparseArray);
+SortLib.quickSort(sparseArray);

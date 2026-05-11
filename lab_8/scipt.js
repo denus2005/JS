2be0e2a22718f3653fddd4390ada17bc825a0913
denus2005 <150010370+denus2005@users.noreply.github.com>
2026-05-11 21:52:32 +0300

    Create scipt.js

---
+++ lab_8/scipt.js
@@ -0,0 +1,58 @@
+// MENU
+
+function toggleMenu() {
+
+    document
+        .getElementById("menu")
+        .classList
+        .toggle("show");
+}
+
+// CAROUSEL
+
+let index = 0;
+
+const slides = document.getElementById("slides");
+
+const dots = document.querySelectorAll(".dot");
+
+const totalSlides = document.querySelectorAll(".slide").length;
+
+function updateCarousel() {
+
+  slides.style.transform = translateX(-${index * 100}%);
+
+    dots.forEach(dot =>
+        dot.classList.remove("active")
+    );
+
+    dots[index].classList.add("active");
+}
+
+function moveSlide(step) {
+
+    index += step;
+
+    if (index >= totalSlides) {
+        index = 0;
+    }
+
+    if (index < 0) {
+        index = totalSlides - 1;
+    }
+
+    updateCarousel();
+}
+
+function currentSlide(n) {
+
+    index = n;
+
+    updateCarousel();
+}
+
+// AUTO SLIDE
+
+setInterval(() => {
+    moveSlide(1);
+}, 5000);
